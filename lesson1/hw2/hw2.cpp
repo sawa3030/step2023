f23e6b168fa9a7b6b2a6ec30c21110a8f5588ec3
sawa3030 <esawada2003@gmail.com>
2023-05-18 21:19:26 +0900

    提出版

--- lesson1/hw2/hw2.cpp
+++ lesson1/hw2/hw2.cpp
@@ -31,78 +31,6 @@ map<char, int> score_map = {
     {'z', 4}
 };
 
-map<char, int> alphabet_number = {
-    {'a', 0},
-    {'b', 1},
-    {'c', 2},
-    {'d', 3},
-    {'e', 4},
-    {'f', 5},
-    {'g', 6},    
-    {'h', 7},
-    {'i', 8},
-    {'j', 9},
-    {'k', 10},
-    {'l', 11},
-    {'m', 12},
-    {'n', 13},
-    {'o', 14},
-    {'p', 15},
-    {'q', 16},
-    {'r', 17},
-    {'s', 18},
-    {'t', 19},
-    {'u', 20},
-    {'v', 21},
-    {'w', 22},
-    {'x', 23},
-    {'y', 24},
-    {'z', 25}
-};
-
-vector<vector<int>> make_new_dictionary (vector<string> dictionary) {
-    vector<vector<int>> number_table(dictionary.size(), vector<int>(27));
-
-    for(long unsigned int i = 0; i < dictionary.size(); i++) {
-        int score = 0; 
-        for(long unsigned int j = 0; j < dictionary.at(i).length(); j++) {
-            char character = dictionary.at(i).at(j);
-            number_table.at(i).at(alphabet_number.at(character)) ++;
-            score += score_map.at(character);
-        }
-        number_table.at(i).at(26) = score;
-    }
-
-    return number_table;
-}
-
-int better_solution(string random_word, vector<vector<int>> new_dictionary) {
-    vector<int> counted_random_word(26);
-
-    for(long unsigned int i = 0; i < random_word.size(); i++) {
-        char character = random_word.at(i);
-        counted_random_word.at(alphabet_number.at(character))++;
-    }
-
-    int ans_score = 0;
-    int ans_num = -1;
-    for(long unsigned int i = 0; i < new_dictionary.size(); i++) {
-        bool ans = true;
-        for(int j = 0 ; j < 26; j++) {
-            if(new_dictionary.at(i).at(j) > counted_random_word.at(j)) {
-                ans = false;
-                break;
-            }
-        }
-        if(ans && new_dictionary.at(i).at(26) > ans_score) {
-            ans_num = static_cast<int>(i);
-            ans_score = new_dictionary.at(i).at(26);
-        }
-    }
-
-    return ans_num;
-}
-
 struct counted_word {
     string word;
     vector<int> num_of_char;
@@ -120,9 +48,9 @@ vector<counted_word> make_newer_dictionary (vector<string> dictionary) {
         vector<int> number_table (26, 0);
         int score = 0; 
         for(long unsigned int j = 0; j < dictionary.at(i).length(); j++) {
-            char character = dictionary.at(i).at(j);
-            number_table.at(alphabet_number.at(character)) ++;
-            score += score_map.at(character);
+            int char_num = dictionary.at(i).at(j) - 'a';
+            number_table.at(char_num) ++;
+            score += score_map.at(dictionary.at(i).at(j));
         }
         new_dictionary.at(i).word = dictionary.at(i);
         new_dictionary.at(i).num_of_char = number_table;
@@ -138,11 +66,10 @@ string faster_solution(string random_word, vector<counted_word> new_dictionary) 
     vector<int> counted_random_word(26);
 
     for(long unsigned int i = 0; i < random_word.size(); i++) {
-        char character = random_word.at(i);
-        counted_random_word.at(alphabet_number.at(character))++;
+        int char_num = random_word.at(i) - 'a';
+        counted_random_word.at(char_num)++;
     }
 
-    int ans_num = -1;
     for(long unsigned int i = 0; i < new_dictionary.size(); i++) {
         bool ans = true;
         for(int j = 0 ; j < 26; j++) {
@@ -152,7 +79,7 @@ string faster_solution(string random_word, vector<counted_word> new_dictionary) 
             }
         }
         if(ans) {
-            return new_dictionary.at(i).word;
+            return new_dictionary.at(i).word;      
         }
     }
 
@@ -177,33 +104,8 @@ int main () {
         old_dictionary.push_back(reading_line);
     }
 
-    /*
-    vector<vector<int>> new_dictionary = make_new_dictionary(old_dictionary);  
-
-    //出力
-    ofstream output("medium_answer.txt");
-    for(unsigned long int i = 0; i < random_word.size(); i++) {
-        int ans_num = better_solution(random_word.at(i), new_dictionary);
-
-        if(ans_num == -1){
-            output << "no answer" << endl;
-        } else {
-            string ans = old_dictionary.at(ans_num);
-            output << ans << endl;
-        }
-    }
-    */    
-
     vector<counted_word> newer_dictionary = make_newer_dictionary(old_dictionary);
     
-    for(unsigned long int i = 0; i < newer_dictionary.size(); i++) {
-        cout << newer_dictionary.at(i).word << " ";
-        for (int j = 0; j < 26; j++) {
-            cout << newer_dictionary.at(i).num_of_char.at(j) << " ";
-        }
-        cout << newer_dictionary.at(i).score << endl;
-    }
-
     ofstream output("medium_answer.txt");
     for(unsigned long int i = 0; i < random_word.size(); i++) {
         string ans = faster_solution(random_word.at(i), newer_dictionary);
