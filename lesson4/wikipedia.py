c25abaf45e8d08c5b744a3cf289eeaa5e145c444
sawa3030 <esawada2003@gmail.com>
2023-06-08 11:33:57 +0900

    added find_something_more_interesting

--- lesson4/wikipedia.py
+++ lesson4/wikipedia.py
@@ -117,6 +117,9 @@ class Wikipedia:
                         for i in range (len(ans_path)):
                             print(self.titles[int(ans_path[i])])
                         print()
+                        return
+        
+        print("no path from", start, "to", goal)
         #------------------------#
         pass
 
@@ -128,30 +131,30 @@ class Wikipedia:
         return True
 
     def calculate_page_ranks(self, prev_page_ranks):
-        print("1", time.time())
+        # print("1", time.time())
         page_ranks = {key: 0.15 for key in self.titles}
-        print("2", time.time())
+        # print("2", time.time())
 
         # print(initial_page_ranks)
         # print(page_ranks)
             
-        print("3", time.time())
+        # print("3", time.time())
         distribute_all = 0
         for node_index in self.titles:
             if(len(self.links[node_index]) != 0):
                 distribute_to_children = prev_page_ranks[node_index] * 0.85 / len(self.links[node_index])
                 for child_node_index in self.links[node_index]:
                     page_ranks[child_node_index] += distribute_to_children
-                print("隣接ノードあり", time.time())
+                # print("隣接ノードあり", time.time())
             else:
                 distribute_all += prev_page_ranks[node_index] * 0.85
-                print("隣接ノードなし", time.time())
-        print("4", time.time())
+                # print("隣接ノードなし", time.time())
+        # print("4", time.time())
         
         for node_index in self.titles:
             page_ranks[node_index] += distribute_all / len(self.titles)
-            print(type(prev_page_ranks[node_index]))
-        print("5", time.time())
+            # print(type(prev_page_ranks[node_index]))
+        # print("5", time.time())
         
         # print(initial_page_ranks)
         # print(page_ranks)
@@ -195,11 +198,54 @@ class Wikipedia:
         #------------------------#
         pass
 
-
     # Do something more interesting!!
-    def find_something_more_interesting(self):
+    def find_something_more_interesting(self, start, goal):
         #------------------------#
         # Write your code here!  #
+        if (start == goal):
+            return
+
+        for id in self.titles.keys():
+            if(self.titles[id] == start):
+                start_index = id
+                break
+
+        for id in self.titles.keys():
+            if(self.titles[id] == goal):
+                goal_index = id
+                break
+
+        used = {}
+
+        path_exists = True
+        while(path_exists):
+            path_exists = False
+            queue = collections.deque()
+            paths = {}
+
+            queue.append(start_index)
+            paths[start_index] = str(start_index)
+
+            while(len(queue) != 0):
+                node = queue.popleft()
+                
+                for child_node in self.links[node]:
+                    if(paths.get(child_node) == None and used.get(child_node) == None):
+                        queue.append(child_node)
+                        paths[child_node] = paths[node] + "," + str(child_node)
+
+                        if(child_node == goal_index):
+                            print("The shortest pass from", start, "to", goal, "is")
+                            ans_path = paths[child_node].split(",")
+                            for i in range (len(ans_path)):
+                                print(self.titles[int(ans_path[i])])
+                                if not i == len(ans_path) - 1:
+                                    used[int(ans_path[i])] = True
+                            print()
+                            path_exists = True
+                        
+        
+        print("no path from", start, "to", goal)
         #------------------------#
         pass
 
@@ -214,3 +260,4 @@ if __name__ == "__main__":
     # wikipedia.find_most_linked_pages()
     # wikipedia.find_shortest_path("渋谷", "パレートの法則")
     # wikipedia.find_most_popular_pages()
+    wikipedia.find_something_more_interesting("渋谷", "パレートの法則")
